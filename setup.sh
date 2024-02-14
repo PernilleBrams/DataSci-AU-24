ad75fec9acbafedd2dba632d64af77b4bfda7981
PernilleBrams <pernille@brams.dk>
2024-02-14 08:30:47 +0100

    added files

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# github setup
+git config --global user.email "pernille@brams.dk"
+git config --global user.name "PernilleBrams"
+
+pip install --upgrade pip
+
+source "/work/PernilleHøjlundBrams#8577/DataSci-AU-24/venvs/data_sci_2024/bin/activate"
+
+pip install --upgrade pip
+
+pip install ipykernel
+
+python3 -m ipykernel install --user --name=data_sci_2024
